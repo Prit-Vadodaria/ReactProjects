a8aadfc066d4cc53ac169696cfdda43800df6bbb
Prit Vadodaria <pritvadodaria16@gmail.com>
2026-01-12 19:56:50 +0530

    Disable buttons when no text is present
    Added disable functionality to buttons based on word count.

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -3,20 +3,25 @@ import React,{useState} from 'react'
 export default function TextForm(props) {
     //TextArea
     const[text,setText] = useState('');
+    const wordCount = text.trim()? text.trim().split(/\s+/).length : 0;
+    const disable = wordCount===0 ? true : false ;
     const setUpper = () =>
     {
+         
         let newText=text.toUpperCase();
         setText(newText);
         props.showAlert("Converted To UpperCase","success");
     }
     const setLower = () =>
     {
+         
         let newText=text.toLowerCase();
         setText(newText);
         props.showAlert("Converted To Lowercase","success");
     }
     const setCap = () =>
     {
+         
         const words=text.split(" ");
         const capitalizedwords = words.map( word =>
             {
@@ -33,26 +38,31 @@ export default function TextForm(props) {
     }
     const Copytxt = () =>
     {
-        var text = document.getElementById("FormControlTextarea");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+         
+        var cptext = document.getElementById("FormControlTextarea");
+        cptext.select();
+        navigator.clipboard.writeText(cptext.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text Copied","success");
 
     }
     const rmSpaces = () =>
     {
+         
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
         props.showAlert("Spaces Removed","success");
     }
     const setCommaSep = () =>
     {
+         
         let newText = text.split(/[ ]+/);
         setText(newText.join(","));
         props.showAlert("Space has been Replaced with a Comma","success");
     }
     const setClear = () => 
     {
+         
         setText("");
         props.showAlert("Text Cleared","danger");
     }
@@ -100,7 +110,6 @@ export default function TextForm(props) {
     {
         setReplacedText(event.target.value);
     }
-    const wordCount = text.trim()? text.trim().split(/\s+/).length : 0;
     return (
         <>
             <div className="container">
@@ -108,27 +117,27 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" id="FormControlTextarea" rows="8" value={text} onChange={setTxtValue} style={{background:props.mode === 'light'?'white':'#212529' ,color:props.mode==='light'?'black':'white'}}></textarea>
                 </div>
-                <button className='btn btn-primary my-2 mx-1' onClick={setUpper}>UpperCase</button>
-                <button className='btn btn-primary my-2 mx-1' onClick={setLower}>LowerCase</button>
-                <button className='btn btn-primary my-2 mx-1' onClick={setCap}>Capitalise</button>
-                <button className='btn btn-primary my-2 mx-1' onClick={setCommaSep}>Convert to Comma Sep</button>
-                <button className='btn btn-primary my-2 mx-1' onClick={Copytxt}>Copy Text</button>
-                <button className='btn btn-primary my-2 mx-1' onClick={rmSpaces}>Remove Extra Spaces</button>
-                <button className='btn btn-danger my-2 mx-1' onClick={setClear}>Clear Text</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={setUpper}>UpperCase</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={setLower}>LowerCase</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={setCap}>Capitalise</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={setCommaSep}>Convert to Comma Sep</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={Copytxt}>Copy Text</button>
+                <button disabled={disable} className='btn btn-primary my-2 mx-1' onClick={rmSpaces}>Remove Extra Spaces</button>
+                <button disabled={disable} className='btn btn-danger my-2 mx-1' onClick={setClear}>Clear Text</button>
             </div>
             <hr></hr>
             <div className="container">
                 <h3>Replace:</h3>
-                <form class="row g-3">
-                    <div class="col-auto">
-                        <input type="text" class="form-control" id="inputText2" style={{background:props.mode==='light'?'white':'#212529' ,color:props.mode==='light'?'black':'white'}} value={replacedtext} onChange={setreplacedtextValue}/>
+                <form className="row g-3">
+                    <div className="col-auto">
+                        <input type="text" className="form-control" id="inputText2" style={{background:props.mode==='light'?'white':'#212529' ,color:props.mode==='light'?'black':'white'}} value={replacedtext} onChange={setreplacedtextValue}/>
                     </div>
-                    <div class="col-auto">
-                        <input type="text" class="form-control" id="inputText2" style={{background:props.mode==='light'?'white':'#212529' ,color:props.mode==='light'?'black':'white'}} value={replacetext} onChange={setreplacetextValue}/>
+                    <div className="col-auto">
+                        <input type="text" className="form-control" id="inputText2" style={{background:props.mode==='light'?'white':'#212529' ,color:props.mode==='light'?'black':'white'}} value={replacetext} onChange={setreplacetextValue}/>
                     </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-primary mb-3 mx-1" onClick={replaceTxt}>Replace</button>
-                        <button type="button" class="btn btn-primary mb-3 mx-1" onClick={replaceAllTxt}>Replace All</button>                        
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-primary mb-3 mx-1" onClick={replaceTxt}>Replace</button>
+                        <button type="button" className="btn btn-primary mb-3 mx-1" onClick={replaceAllTxt}>Replace All</button>                        
                     </div>
                 </form>
             </div>
@@ -142,7 +151,7 @@ export default function TextForm(props) {
             <hr></hr>
             <div className="container">
                 <h3>Preview</h3>
-                <p className={`border border-${props.mode==='light'?'#e0e4e8':'white'} p-3 rounded-3`}>{text.length>0?text:"Enter Text to Preview It"}</p>
+                <p className={`border border-${props.mode==='light'?'#e0e4e8':'white'} p-3 rounded-3`}>{text.length>0?text:"Nothing to Preview"}</p>
             </div>
             <hr></hr>
         </>
