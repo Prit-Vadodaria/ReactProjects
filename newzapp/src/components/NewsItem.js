2afe8b6999c23d78bf98685722054b7e78895ad8
Prit-Vadodaria <pritvadodaria16@gmail.com>
2026-01-16 18:36:09 +0530

    feat: add sample output data and enhance NewsItem component with dynamic article rendering

--- newzapp/src/components/NewsItem.js
+++ newzapp/src/components/NewsItem.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description , imgUrl , newsUrl} = this.props;
     return (
       <>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+        <div className="card mx-3 my-3" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/NewsDetail" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title.length > 45 ? `${title.slice(0, 44)}...` : title}</h5>
+            <p className="card-text">{description.slice(0,84)}...</p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">
+              Read More
             </a>
           </div>
         </div>
